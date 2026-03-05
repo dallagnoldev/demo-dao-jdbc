8915009c9857499add80552e571f21a1ddc9b18c
Joao Pedro <@dallagnol.joao300@gmail.com>
2026-03-05 17:39:56 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,23 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         Department department = new Department(1, "Books");
 
+        LocalDate birthDate = LocalDate.parse("18/12/2000", dtf);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", birthDate, 3000.0, department);
+
         System.out.println(department);
+        System.out.println(seller);
     }
 }
