7829fb2602b736c09023ffb427773dbbd16f4631
Joao Pedro <@jjoaoxz20@gmail.com>
2026-03-10 19:16:49 -0300

    update implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,6 +12,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== Test 1: seller findById ===");
@@ -31,9 +33,16 @@ public class Main {
         }
 
         System.out.println("\n=== Test 4: seller insert ===");
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.parse("01/01/2000", dtf), 4000.0, new Department(2, null));
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New Id: " + newSeller.getId());
+
+        System.out.println("\n=== Test 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update Completed");
+
     }
 }
